eae47cd784dacb56d42b31f7f281da23ffec59de
syseoz <syseoz@naver.com>
2023-08-07 22:23:46 +0900

    add auth slice

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import authSlice from "./slices/authSlice.jsx";
 
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
+});
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
